c6514a350ca44467d477ad32557fd5703aca24b1
Muhammad Umer Aziz <m.umer394aziz@gmail.com>
2026-03-27 15:41:33 +0500

    cleaned the project from unnecessary things.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
+
+
 import './App.css'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <h1 class="text-3xl font-bold text-red-500 underline text-center my-10">
-yahoo
+        yahoo
       </h1>
 
     </>
