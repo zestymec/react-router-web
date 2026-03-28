8b3c3c09f5a47b4da9cc532503d8f39248030d12
Muhammad Umer Aziz <m.umer394aziz@gmail.com>
2026-03-28 15:12:52 +0500

    app.jsx dlt and footer and header added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -33,8 +33,8 @@ export default function Header() {
                       <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                           <li>
                               <NavLink
-                                  className={() =>
-                                      `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                  className={({isActive}) =>
+                                      `block py-2 pr-4 ${isActive ? "text-amber-700" : "text-gray-600"  } pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
                               >
                                   Home
