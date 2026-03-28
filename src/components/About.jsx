3e6d7e0d3b8785b9cb080b9898f7502877b94b1f
Muhammad Umer Aziz <m.umer394aziz@gmail.com>
2026-03-28 16:26:22 +0500

    about btn added and debugge some mistakes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function About() {
   return (
     <div>
-      About
+     <h1 className='text-center text-2xl sm:text-5xl pt-10 font-medium'> About</h1>
+     <p className='text-center py-2 font-medium'>The name Arslan (also spelled Aslan, Arsalan) is a Turkic masculine given name originating from Old Turkic, translating directly to "lion". It is often used figuratively to denote bravery, fearlessness, and leadership. Throughout history, it has been associated with rulers, warriors, and noble figures across Central Asian, Persian, and Turkish cultures.</p>
     </div>
   )
 }
