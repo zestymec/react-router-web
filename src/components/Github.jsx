45dd8d3e755189e08f3937b890792adc30ad997b
zestymec <m.umer394aziz@gmail.com>
2026-03-30 12:44:54 +0500

    pushing the code

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data , setdata] = useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/zestymec')
+        .then(Response=> Response.json())
+        .then(data=>{
+            setdata(data)
+        })
+    })
   return (
     <div className='text-blue-400 text-6xl bg-amber-700 text-center m-5 p-4'>
-      Github followers :
+      Github followers of Umer :
     </div>
   )
 }
