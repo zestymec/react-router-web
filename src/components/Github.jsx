74cc10cd82cbcbb023f863ea0c77ea84d31885ea
zestymec <m.umer394aziz@gmail.com>
2026-03-30 12:26:45 +0500

    github component added

---
+++ src/components/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Github() {
+  return (
+    <div className='text-blue-400 text-6xl bg-amber-700 text-center'>
+      Github followers ;
+    </div>
+  )
+}
+
+export default Github
